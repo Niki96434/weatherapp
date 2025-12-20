560d28159dd425a713282ec6ba737b7897c75c6f
Niki96434 <cozercaniegirl143@mail.ru>
2025-12-20 15:56:24 +0300

    ff

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -7,8 +7,8 @@ export default function Result({city, temp, weather, max_temp, min_temp, feels_l
         <p className='tempTitle'>{`${Math.ceil(temp)}°C`}</p>
         <span>{weather}</span>
         <div className="maxmintemp">
-            <div>{`Мин.:${Math.ceil(max_temp)}°C `}</div>
-            <div>{`Макс.:${Math.ceil(min_temp)}°C`}</div>
+            <div>{`Макс.:${Math.ceil(max_temp)}°C `}</div>
+            <div>{`Мин.:${Math.ceil(min_temp)}°C`}</div>
         </div>
         <div>{`Ощущается как ${Math.ceil(feels_like)}°C`}</div>
         </>
